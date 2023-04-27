03225d4798bf489475a239aa3f814bdfa07cc613
yuliiache <ychebarova@gmail.com>
2023-04-27 15:32:48 -0700

    Practiced building new components inside the app component and rendering it inside the return statement

---
+++ firstapp/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+import Heading from './Heading';
+
+function App() {
+    return (
+        <div className="App">
+            <Heading />
+        </div>
+    );
+}
+
+export default App;
---
+++ firstapp/src/Heading.js
@@ -0,0 +1,7 @@
+function Heading() {
+    return (
+        <h1>Hello, </h1>
+    )
+}
+
+export default Heading;
