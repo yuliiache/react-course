dae2a97208b138403ca8f945522eee7887805ece
yuliiache <ychebarova@gmail.com>
2023-06-15 17:31:20 -0700

    Created handleDecreaseClick function to decrease the count of the corresponding product / remove it from the list (if count is 0) if minus button is clicked

---
+++ shop-item-remove/src/App.js
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+
+const initialProducts = [{
+  id: 0,
+  name: 'Baklava',
+  count: 1,
+}, {
+  id: 1,
+  name: 'Cheese',
+  count: 5,
+}, {
+  id: 2,
+  name: 'Spaghetti',
+  count: 2,
+}];
+
+export default function ShoppingCart() {
+  const [
+    products,
+    setProducts
+  ] = useState(initialProducts)
+
+  function handleIncreaseClick(productId) {
+    setProducts(products.map(product => {
+      if (product.id === productId) {
+        return {
+          ...product,
+          count: product.count + 1
+        };
+      } else {
+        return product;
+      }
+    }))
+  }
+  function handleDecreaseClick(productId) {
+    setProducts(products.map(product => {
+      if (product.id === productId) {
+        if ( product.count >= 2) {
+          return {
+            ...product,
+            count: product.count - 1
+          };
+        } else {
+          return null
+        }
+      } else {
+        return product;
+      }
+    }).filter(product => product !== null));
+  }
+
+  return (
+      <ul>
+        {products.map(product => (
+            <li key={product.id}>
+              {product.name}
+              {' '}
+              (<b>{product.count}</b>)
+              <button onClick={() => {
+                handleIncreaseClick(product.id);
+              }}>
+                +
+              </button>
+              <button onClick={() => {
+                handleDecreaseClick(product.id);
+              }}>
+                –
+              </button>
+            </li>
+        ))}
+      </ul>
+  );
+}
