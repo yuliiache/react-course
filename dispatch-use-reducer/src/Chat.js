af75b5c5c077a35a0dfe625591772f19f87c7649
yuliiache <ychebarova@gmail.com>
2023-07-14 15:56:51 -0700

    Fixed ContactList and Chat with the code to dispatch the corresponding actions.

---
+++ dispatch-use-reducer/src/Chat.js
@@ -0,0 +1,21 @@
+// import { useState } from 'react';
+
+export default function Chat({contact, message, dispatch}) {
+    return (
+        <section className="chat">
+      <textarea
+          value={message}
+          placeholder={'Chat to ' + contact.name}
+          onChange={(e) => {
+              dispatch({
+                  type: 'edited_message',
+                  contact: contact,
+                  message: e.target.value,
+              })
+          }}
+      />
+            <br />
+            <button>Send to {contact.email}</button>
+        </section>
+    );
+}
