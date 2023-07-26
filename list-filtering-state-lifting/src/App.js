f717c4c7fad2f62cc1aec58703e38f11fa7dacc2
yuliiache <ychebarova@gmail.com>
2023-07-25 17:13:26 -0700

    fixed the search by lifting state from SearchBar to FilterableList component and giving the filtered result to List as props

---
+++ list-filtering-state-lifting/src/App.js
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { foods, filterItems } from './data.js';
+
+export default function FilterableList() {
+  const [query, setQuery] = useState('');
+  const results = filterItems(foods, query);
+    function handleChange(e) {
+        setQuery(e.target.value);
+    }
+
+  return (
+      <>
+        <SearchBar
+            query={query}
+            handleChange={handleChange}/>
+        <hr />
+        <List items={results} />
+      </>
+  );
+}
+
+function SearchBar({query, handleChange}) {
+
+  return (
+      <label>
+        Search:{' '}
+        <input
+            value={query}
+            onChange={handleChange}
+        />
+      </label>
+  );
+}
+
+function List({ items }) {
+  return (
+      <table>
+        <tbody>
+        {items.map(food => (
+            <tr key={food.id}>
+              <td>{food.name}</td>
+              <td>{food.description}</td>
+            </tr>
+        ))}
+        </tbody>
+      </table>
+  );
+}
