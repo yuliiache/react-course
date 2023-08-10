116aab836a5d2421ac9e570ee782e1a03cde3f05
yuliiache <ychebarova@gmail.com>
2023-08-10 12:25:21 -0700

    Little fix to change status to try again if no one wins

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,8 +1,13 @@
-import {useState} from "react";
+import { useState } from "react";
 import React from "react";
 
-function Square({ value, onSquareClick }) {
-    return <button className="square" onClick={onSquareClick}>
+function Square({ value, onSquareClick, isWinner }) {
+    let buttonColor = '';
+
+    if(isWinner) {
+        buttonColor = 'lightskyblue'
+    }
+    return <button className="square" onClick={onSquareClick} style={{ backgroundColor: buttonColor}}>
         {value}
     </button>;
 }
@@ -24,32 +29,41 @@ function Board({ xIsNext, squares, onPlay }) {
     const winner = calculateWinner(squares);
     let status;
     if (winner) {
-        status = 'Winner: ' + winner;
+        status = 'Winner: ' + squares[winner[0]];
+    } else if (!squares.includes(null)) {
+        status = 'Try again';
     } else {
         status = 'Next player: ' + (xIsNext ? 'X' : '0');
     }
 
+    const boardSize = 3;
+    let board = [];
+    for (let i = 0; i < boardSize; i++) {
+        let row = [];
+        for (let j = 0; j < boardSize; j++) {
+            const squareIndex = i * boardSize + j
+            let isWinner = false;
+            if (winner && winner.includes(squareIndex)) {
+                isWinner = true;
+            }
+            row.push(
+                <Square
+                    value={squares[squareIndex]}
+                    onSquareClick={() => handleClick(squareIndex)}
+                    isWinner={isWinner}
+                    key={squareIndex}
+                />
+            );
+        }
+        board.push(<div className='board-row' key={i}>{row}</div>);
+    }
 
-  return(
-      <>
-        <div className='status'>{status}</div>
-        <div className='board-row'>
-            <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-            <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-            <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-        </div>
-        <div className='board-row'>
-            <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-            <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-            <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-        </div>
-        <div className='board-row'>
-            <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-            <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-            <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-        </div>
-      </>
-  )
+    return(
+        <>
+            <div className='status'>{status}</div>
+            {board}
+        </>
+    )
 }
 
 export default function Game() {
@@ -63,7 +77,6 @@ export default function Game() {
 
     function handlePlay(nextSquares) {
         const nextHistory = [...history.slice(0, currentMove + 1), {data:nextSquares, id: currentMove + 1}];
-        console.log(nextHistory)
         setHistory(nextHistory);
         setCurrentMove(nextHistory.length - 1);
     }
@@ -102,7 +115,6 @@ export default function Game() {
 
     function handleToggle() {
         setIsDescending(!isDescending)
-        console.log("toggled")
     }
 
     const sortedMoves = [...moves];
@@ -139,8 +151,9 @@ function calculateWinner(squares) {
     for (let i = 0; i < lines.length; i++) {
         const [a, b, c] = lines[i];
         if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
+            return lines[i];
         }
     }
     return null;
+
 }
